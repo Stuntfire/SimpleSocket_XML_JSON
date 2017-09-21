36d73f5955c8bb0e2388c59edbe5375ae1add609
Søren Roed Schack <soer622t@edu.easj.dk>
2017-09-21 10:13:52 +0200

    Assignment B: XML Communication Create a new Project 'ConsoleApp (.Net framework)', and name it e.g. 'XmlServer'. In the XmlServer Create a class 'Server' with a method 'Start'. In the start method Listen on port 10002. When a client connect - read from the socket a text string(xml) representing a car and print it out. Decode the xml-string into a car object and print out the car-object.

--- XmlServer/Program.cs
+++ XmlServer/Program.cs
@@ -6,10 +6,18 @@ using System.Threading.Tasks;
 
 namespace XmlServer
 {
+    /// <summary>
+    /// In the XmlServer Create a class 'Server' with a method 'Start'.
+    /// In the start method Listen on port 10002
+    /// When a client connect - read from the socket a text string (xml) representing a car and print it out.
+    /// Decode the xml-string into a car object and print out the car-object.
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
+            Server server = new Server();
+            server.Start();
         }
     }
 }
---
+++ XmlServer/Server.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+using ModelLib;
+
+namespace XmlServer
+{
+    /// <summary>
+    /// In the XmlServer Create a class 'Server' with a method 'Start'.
+    /// In the start method Listen on port 10002
+    /// When a client connect - read from the socket a text string (xml) representing a car and print it out.
+    /// Decode the xml-string into a car object and print out the car-object.
+    /// </summary>
+    
+    public class Server
+    {
+        public void Start()
+        {
+            TcpListener serverListener = new TcpListener(IPAddress.Loopback, 10002);
+            serverListener.Start();
+            Console.WriteLine("Server startet ...");
+
+            while (true)
+            {
+                TcpClient client = serverListener.AcceptTcpClient();
+                NetworkStream connectToClientStream = client.GetStream();
+                StreamReader xmLfromClientReader = new StreamReader(connectToClientStream);
+
+                Console.WriteLine($"Printer xml-string modtaget fra Client: {xmLfromClientReader}");
+
+                XmlSerializer xs = new XmlSerializer(typeof(Car));
+                Car tempCar = (Car)xs.Deserialize(xmLfromClientReader);
+                Console.WriteLine(tempCar);
+            }
+        }
+    }
+}
